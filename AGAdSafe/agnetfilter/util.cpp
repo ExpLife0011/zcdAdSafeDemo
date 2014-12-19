f670d9a309d783449048555a40ead131a3217518
kevinzhwl <kevinzhwl@gmail.com>
2014-12-19 18:20:43 +0800

    增加临时代码

--- AGAdSafe/agnetfilter/util.cpp
+++ AGAdSafe/agnetfilter/util.cpp
@@ -49,3 +49,82 @@ void WriteAGLog(LPCSTR cstr)
 	}
 
 }
+
+
+// DLL_Injection.cpp : 定义 DLL 应用程序的导出函数。
+//
+ 
+#include "stdafx.h"
+//#include "DLL_Injection.h"
+#include <process.h>
+#include <list>
+#include <vector>
+#include <TlHelp32.h>
+std::list<DLL> Dll_List;
+ 
+extern DWORD EventSize;
+ 
+//提升进程权限
+bool EnableDebugPrivilege(const LPTSTR name)
+{
+     HANDLE token;
+     TOKEN_PRIVILEGES tp;
+    //打开进程令牌环
+     if(!OpenProcessToken(GetCurrentProcess(),
+        TOKEN_ADJUST_PRIVILEGES|TOKEN_QUERY,&token))
+   {
+      return false;
+   }
+   //获得进程本地唯一ID
+    LUID luid;
+    if(!LookupPrivilegeValue(NULL,name,&luid))
+    {
+         return false;
+    }
+    tp.PrivilegeCount=1;
+    tp.Privileges[0].Attributes=SE_PRIVILEGE_ENABLED;
+    tp.Privileges[0].Luid=luid;
+    //调整进程权限
+    if(!AdjustTokenPrivileges(token,0,&tp,sizeof(TOKEN_PRIVILEGES),NULL,NULL))
+    {
+        return false;
+    }
+    return true;
+}
+     
+bool LoadDllToProcess(TCHAR *Path,DWORD Id)
+{
+    EnableDebugPrivilege(SE_DEBUG_NAME);
+    HANDLE Process=OpenProcess(PROCESS_ALL_ACCESS,FALSE,Id);
+    if(Process==NULL)
+    {
+        return false;
+    }
+    else
+    {
+        size_t Size=wcslen(Path)*sizeof(TCHAR)+1;
+        BYTE *Param=(BYTE *)VirtualAllocEx(Process,NULL,Size,MEM_COMMIT,PAGE_READWRITE);
+        if(Param)
+        {
+            if(WriteProcessMemory(Process,Param,Path,Size,NULL))
+            {
+                VirtualProtectEx(Process,Param,Size,PAGE_READONLY,NULL);
+                if(CreateRemoteThread(Process,NULL,0,(LPTHREAD_START_ROUTINE)LoadLibrary,
+                                     Param,NULL,NULL)==NULL)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                CloseHandle(Process);
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
